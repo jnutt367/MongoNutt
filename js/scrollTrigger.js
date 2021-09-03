b5847efc0f4266442f52cabbe34f46d9443d1478
Jason Nutt <jnutt367@gmail.com>
2021-09-03 09:51:52 -0400

    added another row of content boxes that need refactored yet

--- js/scrollTrigger.js
+++ js/scrollTrigger.js
@@ -11,9 +11,9 @@ let tl = gsap.timeline({
             toggleActions: "restart none none restart"
         }
     })
-    .to("#local", { scale: 2, fill: "green", ease: "elastic", duration: 3 })
-    .to("#atlas", { scale: 3, fill: "black", ease: "elastic", duration: 3 }, 1)
-    .to("#compass", { scale: 3, fill: "green", ease: "elastic", duration: 3, rotate: 45 }, 1.5)
+    .to("#local", { scale: 3, fill: "green", ease: "elastic", padding: "0.1rem", margin: "0.5rem", duration: 3 }, 4)
+    .to("#atlas", { scale: 3, fill: "black", ease: "elastic", duration: 3 }, 4)
+    .to("#compass", { scale: 3, fill: "green", ease: "elastic", duration: 3, rotate: 45 }, 4)
     .fromTo("#next-react ", { opacity: 0, y: 100 }, { y: -20, duration: 2, ease: "elastic", opacity: 1 }, 0)
     .fromTo("#man", { opacity: 0 }, { y: -20, duration: 7, ease: "elastic", opacity: 1 }, 2)
     .fromTo("#go", { x: -300, opacity: 0 }, { x: 0, y: -20, duration: 3, ease: "elastic", opacity: 1 }, 2)
