b2fb2b2691483a2299098f1374b1dea309668290
Jason Nutt <jnutt367@gmail.com>
2021-09-03 11:36:54 -0400

    commit for replaced dummie text with well though out scripts

--- js/scrollTrigger.js
+++ js/scrollTrigger.js
@@ -11,7 +11,7 @@ let tl = gsap.timeline({
             toggleActions: "restart none none restart"
         }
     })
-    .fromTo("#author", { opacity: 0 }, { opacity: 1, scale: 1.4, color: "green", textShadow: "5px 5px black" })
+    .fromTo("#author", { opacity: 0 }, { opacity: 1, scale: 1.1, color: "green", textShadow: "5px 5px black" })
     .to("#local", { scale: 3, fill: "green", ease: "elastic", padding: "0.1rem", margin: "0.5rem", duration: 3 }, 4)
     .to("#atlas", { scale: 3, fill: "black", ease: "elastic", duration: 3 }, 4)
     .to("#compass", { scale: 3, fill: "green", ease: "elastic", duration: 3, rotate: 45 }, 4)
