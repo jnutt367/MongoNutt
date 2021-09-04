66a473d7f419f4e508c17e1748c23c85432f2f3d
Jason Nutt <jnutt367@gmail.com>
2021-09-04 11:08:49 -0400

    commit for new pages and links

--- js/scrollTrigger.js
+++ js/scrollTrigger.js
@@ -12,7 +12,7 @@ let tl = gsap.timeline({
         }
     })
     .fromTo("#author", { opacity: 0 }, { opacity: 1, scale: 1.1, color: "green", textShadow: "5px 5px black" })
-    .to("#local", { scale: 3, fill: "green", ease: "elastic", padding: "0.1rem", margin: "0.5rem", duration: 3 }, 4)
+    .to("#local", { fill: "green", ease: "elastic", padding: "0.1rem", margin: "0.5rem", duration: 3 }, 4)
     .to("#atlas", { scale: 3, fill: "black", ease: "elastic", duration: 3 }, 4)
     .to("#compass", { scale: 3, fill: "green", ease: "elastic", duration: 3, rotate: 45 }, 4)
     .fromTo("#next-react ", { opacity: 0, y: 100 }, { y: -20, duration: 2, ease: "elastic", opacity: 1 }, 0)
